56cc76a9711fa13a23652b6f477200b35996b07b
redageddon <therealrubiksmaster@gmail.com>
2021-01-16 19:23:33 -0800

    split span and readonly span extensions

---
+++ X10D/src/SpanExtensions/ReadOnlySpanExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    public static partial class SpanExtensions
+    {
+        /// <summary>
+        ///     Fills a buffer randomly with values found in <paramref name="buffer"/>.
+        /// </summary>
+        /// <param name="values">The values being pulled.</param>
+        /// <param name="buffer">The buffer being filled.</param>
+        /// <param name="random">The <see cref="Random"/> instance.</param>
+        /// <typeparam name="T">Any type.</typeparam>
+        public static void Random<T>(this ReadOnlySpan<T> values, Span<T> buffer, Random? random = null)
+        {
+            random ??= RandomExtensions.RandomExtensions.Random;
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = values[random.Next(0, values.Length)];
+            }
+        }
+        
+        /// <summary>
+        ///     Shuffles a <see cref="Span{T}"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="Span{T}"/> to shuffle.</param>
+        /// <param name="random">The <see cref="Random"/> instance.</param>
+        /// <typeparam name="T">Any type.</typeparam>
+        public static ReadOnlySpan<T> Shuffled<T>(this ReadOnlySpan<T> source, Random? random = null)
+        {
+            Span<T> writeableSpan = new(new T[source.Length]);
+            source.CopyTo(writeableSpan);
+            writeableSpan.Shuffle(random);
+
+            return writeableSpan;
+        }
+    }
+}
--- X10D/src/SpanExtensions/SpanExtensions.cs
+++ X10D/src/SpanExtensions/SpanExtensions.cs
@@ -5,25 +5,8 @@ namespace X10D.Performant.SpanExtensions
     /// <summary>
     ///     Extension methods for <see cref="Span{T}"/>.
     /// </summary>
-    public static class SpanExtensions
+    public static partial class SpanExtensions
     {
-        /// <summary>
-        ///     Fills a buffer randomly with values found in <paramref name="buffer"/>.
-        /// </summary>
-        /// <param name="values">The values being pulled.</param>
-        /// <param name="buffer">The buffer being filled.</param>
-        /// <param name="random">The <see cref="Random"/> instance.</param>
-        /// <typeparam name="T">Any type.</typeparam>
-        public static void Random<T>(this ReadOnlySpan<T> values, Span<T> buffer, Random? random = null)
-        {
-            random ??= RandomExtensions.RandomExtensions.Random;
-
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                buffer[i] = values[random.Next(0, values.Length)];
-            }
-        }
-
         /// <inheritdoc cref="Random{T}(ReadOnlySpan{T},Span{T},Random?)"/>
         public static void Random<T>(this Span<T> values, Span<T> buffer, Random? random = null) => Random((ReadOnlySpan<T>)values, buffer, random);
 
@@ -68,21 +51,6 @@ namespace X10D.Performant.SpanExtensions
                 source[count] = source[index];
                 source[index] = temp;
             }
-        }
-
-        /// <summary>
-        ///     Shuffles a <see cref="Span{T}"/>.
-        /// </summary>
-        /// <param name="source">The <see cref="Span{T}"/> to shuffle.</param>
-        /// <param name="random">The <see cref="Random"/> instance.</param>
-        /// <typeparam name="T">Any type.</typeparam>
-        public static ReadOnlySpan<T> Shuffled<T>(this ReadOnlySpan<T> source, Random? random = null)
-        {
-            Span<T> writeableSpan = new(new T[source.Length]);
-            source.CopyTo(writeableSpan);
-            writeableSpan.Shuffle(random);
-
-            return writeableSpan;
         }
 
         /// <inheritdoc cref="Shuffled{T}(ReadOnlySpan{T},Random?)"/>

0c707a9abfe14656ed70310523d86907140a36e6
redageddon <therealrubiksmaster@gmail.com>
2021-01-16 22:07:16 -0800

    added decimal extensions

---
+++ X10D/src/DecimalExtensions/DecimalExtensions/DecimalExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace X10D.Performant.DecimalExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="decimal"/>.
+    /// </summary>
+    public static class DecimalExtensions
+    {
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.IsEven"/>
+        public static bool IsEven(this decimal value) => value % 2 == 0;
+
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.IsOdd"/>
+        public static bool IsOdd(this decimal value) => value % 2 != 0;
+
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.RadiansToDegrees"/>
+        public static decimal RadiansToDegrees(this decimal angle) => angle * 180.0m / 3.1415926535897932384626433832m;
+
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.DegreesToRadians"/>
+        public static decimal DegreesToRadians(this decimal angle) => 3.1415926535897932384626433832m * angle / 180.0m;
+
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.Round"/>
+        public static decimal Round(this decimal value, decimal nearest = 1) => decimal.Round(value/nearest) * nearest;
+    }
+}

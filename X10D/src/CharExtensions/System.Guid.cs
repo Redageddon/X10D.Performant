807a5dcec08ce763d39daa153712eff57d1b6f26
redageddon <therealrubiksmaster@gmail.com>
2021-01-18 10:42:17 -0800

    re exposed guid

---
+++ X10D/src/CharExtensions/System.Guid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace X10D.Performant.CharExtensions
+{
+    public static partial class CharExtensions
+    {
+        /// <inheritdoc cref="Guid.Parse(ReadOnlySpan{char})" />
+        public static Guid ToGuid(this ReadOnlySpan<char> value) => Guid.Parse(value);
+
+        /// <inheritdoc cref="Guid.ParseExact(ReadOnlySpan{char},ReadOnlySpan{char})" />
+        public static Guid ToGuid(this ReadOnlySpan<char> value, ReadOnlySpan<char> format) => Guid.ParseExact(value, format);
+
+        /// <inheritdoc cref="Guid.TryParse(ReadOnlySpan{char},out Guid)" />
+        public static bool TryToGuid(this ReadOnlySpan<char> value, out Guid result) => Guid.TryParse(value, out result);
+
+        /// <inheritdoc cref="Guid.TryParseExact(ReadOnlySpan{char},ReadOnlySpan{char},out Guid)" />
+        public static bool ToGuid(this ReadOnlySpan<char> value, ReadOnlySpan<char> format, out Guid result) =>
+            Guid.TryParseExact(value, format, out result);
+    }
+}
---
+++ X10D/src/StringExtension/System.Guid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace X10D.Performant.StringExtension
+{
+    public static partial class StringExtensions
+    {
+        /// <inheritdoc cref="Guid.Parse(string)"/>
+        public static Guid ToGuid(this string value) => Guid.Parse(value);
+        
+        /// <inheritdoc cref="Guid.ParseExact(string,string)"/>
+        public static Guid ToGuid(this string value, string format) => Guid.ParseExact(value, format);
+        
+        /// <inheritdoc cref="Guid.TryParse(string,out Guid)"/>
+        public static bool TryToGuid(this string value, out Guid result) => Guid.TryParse(value, out result);
+        
+        /// <inheritdoc cref="Guid.TryParseExact(string,string,out Guid)"/>
+        public static bool TryToGuid(this string value, string format, out Guid result) => Guid.TryParseExact(value, format, out result);
+    }
+}

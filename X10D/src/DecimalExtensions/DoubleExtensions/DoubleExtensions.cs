cf3d97aea68a4b9127de2062c9afecaf4da4f169
redageddon <therealrubiksmaster@gmail.com>
2021-01-19 13:59:30 -0800

    added gradians and inverse sqrt

--- X10D/src/DecimalExtensions/DoubleExtensions/DoubleExtensions.cs
+++ X10D/src/DecimalExtensions/DoubleExtensions/DoubleExtensions.cs
@@ -26,7 +26,14 @@ namespace X10D.Performant.DoubleExtensions
         /// </summary>
         /// <param name="angle">The angle in radians.</param>
         /// <returns><paramref name="angle"/> in degrees.</returns>
-        public static double RadiansToDegrees(this double angle) => angle * 180.0 / Math.PI;
+        public static double RadiansToDegrees(this double angle) => angle * 180.0 / Math.PI;        
+        
+        /// <summary>
+        ///     Converts <paramref name="angle"/> from radians to gradians.
+        /// </summary>
+        /// <param name="angle">The angle in radians.</param>
+        /// <returns><paramref name="angle"/> in gradians.</returns>
+        public static double RadiansToGradians(this double angle) => angle * 200 / Math.PI;
 
         /// <summary>
         ///     Converts <paramref name="angle"/> from degrees to radians.
@@ -34,5 +41,33 @@ namespace X10D.Performant.DoubleExtensions
         /// <param name="angle">The angle in degrees.</param>
         /// <returns><paramref name="angle"/> in radians.</returns>
         public static double DegreesToRadians(this double angle) => Math.PI * angle / 180.0;
+        
+        /// <summary>
+        ///     Converts <paramref name="angle"/> from degrees to gradians.
+        /// </summary>
+        /// <param name="angle">The angle in degrees.</param>
+        /// <returns><paramref name="angle"/> in gradians.</returns>
+        public static double DegreesToGradians(this double angle) => angle * 10d / 9d;       
+        
+        /// <summary>
+        ///     Converts <paramref name="angle"/> from gradians to degrees.
+        /// </summary>
+        /// <param name="angle">The angle in gradians.</param>
+        /// <returns><paramref name="angle"/> in degrees.</returns>
+        public static double GradiansToDegrees(this double angle) => angle * 9d / 10d;       
+        
+        /// <summary>
+        ///     Converts <paramref name="angle"/> from gradians to radians.
+        /// </summary>
+        /// <param name="angle">The angle in gradians.</param>
+        /// <returns><paramref name="angle"/> in radians.</returns>
+        public static double GradiansToRadians(this double angle) => angle * Math.PI / 200;
+        
+        /// <summary>
+        ///     Takes the inverse square root of <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">A double precision floating point value.</param>
+        /// <returns>1 divided by the square root of <paramref name="value"/>.</returns>
+        public static double InverseSqrt(this double value) => 1 / Math.Sqrt(value);
     }
 }
--- X10D/src/DecimalExtensions/FloatExtensions/FloatExtensions.cs
+++ X10D/src/DecimalExtensions/FloatExtensions/FloatExtensions.cs
@@ -18,8 +18,26 @@ namespace X10D.Performant.FloatExtensions
 
         /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.RadiansToDegrees"/>
         public static float RadiansToDegrees(this float angle) => angle * 180.0f / MathF.PI;
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.RadiansToGradians"/>
+        public static float RadiansToGradians(this float angle) => angle * 200 / MathF.PI;
 
         /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.DegreesToRadians"/>
         public static float DegreesToRadians(this float angle) => MathF.PI * angle / 180.0f;
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.DegreesToGradians"/>
+        public static float DegreesToGradians(this float angle) => angle * 10f / 9f;      
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.InverseSqrt"/>
+        public static float InverseSqrt(this float value) => 1 / MathF.Sqrt(value);
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.GradiansToDegrees"/>
+        public static float GradiansToDegrees(this float angle) => angle * 9f / 10f;       
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.GradiansToRadians"/>
+        public static float GradiansToRadians(this float angle) => angle * MathF.PI / 200;
+        
+        /// <inheritdoc cref="X10D.Performant.DoubleExtensions.DoubleExtensions.InverseSqrt"/>
+        public static double InverseSqrt(this double value) => 1 / Math.Sqrt(value);
     }
 }

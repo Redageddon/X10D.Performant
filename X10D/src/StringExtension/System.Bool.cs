4995b9799d8b90e31336e2924c24757e8bed4e17
redageddon <therealrubiksmaster@gmail.com>
2021-01-19 12:11:52 -0800

    upgraded bool parsing speed

--- X10D/src/StringExtension/System.Bool.cs
+++ X10D/src/StringExtension/System.Bool.cs
@@ -3,11 +3,62 @@
     public static partial class StringExtensions
     {
         /// <inheritdoc cref="bool.Parse(string)"/>
-        public static bool ToBool(this string value) =>
-            bool.Parse(value);
-        
+        public static bool ToBool(this string value)
+        {
+            switch (value)
+            {
+                case "TRUE":
+                case "True":
+                case "true":
+                case "T":
+                case "t":
+                case "YES":
+                case "Yes":
+                case "yes":
+                case "Y":
+                case "y":
+                case "1":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
         /// <inheritdoc cref="bool.TryParse(string,out bool)"/>
-        public static bool TryToBool(this string value, out bool result) =>
-            bool.TryParse(value, out result);
+        public static bool TryToBool(this string value, out bool result)
+        {
+            switch (value)
+            {
+                case "TRUE":
+                case "True":
+                case "true":
+                case "T":
+                case "t":
+                case "YES":
+                case "Yes":
+                case "yes":
+                case "Y":
+                case "y":
+                case "1":
+                    result = true;
+                    return true;
+                case "FALSE":
+                case "False":
+                case "false":
+                case "F":
+                case "f":
+                case "NO":
+                case "No":
+                case "no":
+                case "N":
+                case "n":
+                case "0":
+                    result = false;
+                    return true;
+                default:
+                    result = false;
+                    return false;
+            }
+        }
     }
 }

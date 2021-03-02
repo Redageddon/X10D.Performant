ca4e138c06fd0d185b1a5ba3d9b54a0d7674ec4a
redageddon <therealrubiksmaster@gmail.com>
2021-03-01 17:06:24 -0800

    added bytes to string converter

---
+++ X10D.Performant/src/ReExposed/IntegerExtensions/ByteExtensions/System.Encoding.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace X10D.Performant.ReExposed
+{
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static partial class ByteExtensions
+    {
+        /// <inheritdoc cref="Encoding.GetString(byte[])"/>
+        public static string GetString(this byte[] bytes, Encoding? encoding = null) => (encoding ?? Encoding.Default).GetString(bytes);
+    }
+}

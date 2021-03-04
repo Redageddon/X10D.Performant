c5a51894b7a02786f5e64fe278614e5eacd04245
redageddon <therealrubiksmaster@gmail.com>
2021-03-04 12:29:15 -0800

    added missing this

--- X10D.Performant/src/ReExposed/StringExtensions/System.Activator.cs
+++ X10D.Performant/src/ReExposed/StringExtensions/System.Activator.cs
@@ -22,7 +22,7 @@ namespace X10D.Performant.ReExposed
             Activator.CreateInstance(assemblyName, typeName, ignoreCase, bindingFlags, binder, args, culture, activationAttributes);
 
         /// <inheritdoc cref="Activator.CreateInstance(string,string,bool,BindingFlags,Binder,object[],CultureInfo,object[])"/>
-        public static ObjectHandle? CreateInstanceFrom(string assemblyFile,
+        public static ObjectHandle? CreateInstanceFrom(this string assemblyFile,
                                                        string typeName,
                                                        bool ignoreCase = false,
                                                        BindingFlags bindingFlags = BindingFlags.Default,

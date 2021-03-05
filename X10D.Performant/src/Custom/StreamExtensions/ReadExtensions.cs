8268ecd54f1778ccbb59cd70bf8ab9362cc71b63
redageddon <therealrubiksmaster@gmail.com>
2021-03-05 12:25:18 -0800

    Added write methods and split them into separate files

---
+++ X10D.Performant/src/Custom/StreamExtensions/ReadExtensions.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace X10D.Performant
+{
+    public static partial class StreamExtensions
+    {
+        private const int SizeOfDecimal = sizeof(decimal);
+        private const int SizeOfDouble = sizeof(double);
+        private const int SizeOfInt64 = sizeof(long);
+        private const int SizeOfUInt64 = sizeof(ulong);
+        private const int SizeOfSingle = sizeof(float);
+        private const int SizeOfInt32 = sizeof(int);
+        private const int SizeOfUInt32 = sizeof(uint);
+        private const int SizeOfInt16 = sizeof(short);
+        private const int SizeOfUInt16 = sizeof(ushort);
+        private const int SizeOfArgbColor = 4;
+
+        /// <summary>
+        ///     Reads an argb <see cref="Color"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="Color"/> from read data.</returns>
+        public static Color ReadArgbColor(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfArgbColor];
+            stream.Read(buffer);
+
+            return Color.FromArgb(littleEndian
+                                      ? BinaryPrimitives.ReadInt32LittleEndian(buffer)
+                                      : BinaryPrimitives.ReadInt32BigEndian(buffer));
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="bool"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <returns>A <see cref="bool"/> from read data.</returns>
+        public static bool ReadBoolean(this Stream stream) => stream.ReadByte() != 0;
+
+        /// <summary>
+        ///     Reads a <see cref="decimal"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="decimal"/> from read data.</returns>
+        public static decimal ReadDecimal(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfDecimal];
+            stream.Read(buffer);
+
+            int lo;
+            int mid;
+            int hi;
+            int flags;
+
+            if (littleEndian)
+            {
+                lo = BinaryPrimitives.ReadInt32LittleEndian(buffer);
+                mid = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(4));
+                hi = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(8));
+                flags = BinaryPrimitives.ReadInt32LittleEndian(buffer.Slice(12));
+            }
+            else
+            {
+                lo = BinaryPrimitives.ReadInt32BigEndian(buffer.Slice(12));
+                mid = BinaryPrimitives.ReadInt32BigEndian(buffer.Slice(8));
+                hi = BinaryPrimitives.ReadInt32BigEndian(buffer.Slice(4));
+                flags = BinaryPrimitives.ReadInt32BigEndian(buffer);
+            }
+
+            Span<int> bits = stackalloc int[]
+            {
+                lo, mid, hi, flags,
+            };
+
+            return new decimal(bits);
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="double"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="double"/> from read data.</returns>
+        public static double ReadDouble(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfDouble];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadDoubleLittleEndian(buffer)
+                : BinaryPrimitives.ReadDoubleBigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="short"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="short"/> from read data.</returns>
+        public static short ReadInt16(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt16];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadInt16LittleEndian(buffer)
+                : BinaryPrimitives.ReadInt16BigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads an <see cref="int"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>An <see cref="int"/> from read data.</returns>
+        public static int ReadInt32(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt32];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadInt32LittleEndian(buffer)
+                : BinaryPrimitives.ReadInt32BigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="long"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="long"/> from read data.</returns>
+        public static long ReadInt64(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt64];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadInt64LittleEndian(buffer)
+                : BinaryPrimitives.ReadInt64BigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads an rgb <see cref="Color"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="Color"/> from read data.</returns>
+        public static Color ReadRgbColor(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfArgbColor];
+            buffer[0] = (byte)stream.ReadByte();
+            buffer[1] = (byte)stream.ReadByte();
+            buffer[2] = (byte)stream.ReadByte();
+            buffer[3] = byte.MaxValue;
+
+            return Color.FromArgb(littleEndian
+                                      ? BinaryPrimitives.ReadInt32LittleEndian(buffer)
+                                      : BinaryPrimitives.ReadInt32BigEndian(buffer));
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="float"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>A <see cref="float"/> from read data.</returns>
+        public static float ReadSingle(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfSingle];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadSingleLittleEndian(buffer)
+                : BinaryPrimitives.ReadSingleBigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads a <see cref="string"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="byteCount">The amount of <see cref="char"/>s to get inside of the string.</param>
+        /// <param name="encoding">The encoding of the string.</param>
+        /// <returns>A <see cref="string"/> from read data of a specified size.</returns>
+        public static string ReadString(this Stream stream, int byteCount, Encoding? encoding = null)
+        {
+            Span<byte> buffer = stackalloc byte[byteCount];
+            stream.Read(buffer);
+
+            return (encoding ?? Encoding.Default).GetString(buffer);
+        }
+
+        /// <summary>
+        ///     Reads an <see cref="ushort"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>An <see cref="ushort"/> from read data.</returns>
+        public static ushort ReadUInt16(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt16];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadUInt16LittleEndian(buffer)
+                : BinaryPrimitives.ReadUInt16BigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads an <see cref="uint"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>An <see cref="uint"/> from read data.</returns>
+        public static uint ReadUInt32(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt32];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadUInt32LittleEndian(buffer)
+                : BinaryPrimitives.ReadUInt32BigEndian(buffer);
+        }
+
+        /// <summary>
+        ///     Reads an <see cref="ulong"/> from a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        /// <returns>An <see cref="ulong"/> from read data.</returns>
+        public static ulong ReadUInt64(this Stream stream, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt64];
+            stream.Read(buffer);
+
+            return littleEndian
+                ? BinaryPrimitives.ReadUInt64LittleEndian(buffer)
+                : BinaryPrimitives.ReadUInt64BigEndian(buffer);
+        }
+    }
+}
---
+++ X10D.Performant/src/Custom/StreamExtensions/WriteExtensions.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace X10D.Performant
+{
+    public static partial class StreamExtensions
+    {
+        /// <summary>
+        ///     Writes an argb <see cref="Color"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="Color"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteArgbColor(this Stream stream, Color value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfArgbColor];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(buffer, value.ToArgb());
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt32BigEndian(buffer, value.ToArgb());
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="bool"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="bool"/> to be written to a stream.</param>
+        public static void WriteBoolean(this Stream stream, bool value) => stream.WriteByte(value ? 1 : 0);
+
+        /// <summary>
+        ///     Writes a <see cref="decimal"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="decimal"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteDecimal(this Stream stream, decimal value, bool littleEndian = true)
+        {
+            int[] bits = decimal.GetBits(value);
+            Span<byte> lo = stackalloc byte[SizeOfInt32];
+            Span<byte> mid = stackalloc byte[SizeOfInt32];
+            Span<byte> hi = stackalloc byte[SizeOfInt32];
+            Span<byte> flags = stackalloc byte[SizeOfInt32];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(lo, bits[0]);
+                BinaryPrimitives.WriteInt32LittleEndian(mid, bits[1]);
+                BinaryPrimitives.WriteInt32LittleEndian(hi, bits[2]);
+                BinaryPrimitives.WriteInt32LittleEndian(flags, bits[3]);
+
+                stream.Write(lo);
+                stream.Write(mid);
+                stream.Write(hi);
+                stream.Write(flags);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt32BigEndian(lo, bits[3]);
+                BinaryPrimitives.WriteInt32BigEndian(mid, bits[2]);
+                BinaryPrimitives.WriteInt32BigEndian(hi, bits[1]);
+                BinaryPrimitives.WriteInt32BigEndian(flags, bits[0]);
+
+                stream.Write(flags);
+                stream.Write(hi);
+                stream.Write(mid);
+                stream.Write(lo);
+            }
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="double"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="double"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void ReadDouble(this Stream stream, double value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfDouble];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteDoubleLittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteDoubleBigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="short"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="short"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteInt16(this Stream stream, short value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt16];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt16LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt16BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="int"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="int"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteInt32(this Stream stream, int value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt32];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="long"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="long"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteInt64(this Stream stream, long value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfInt64];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt64LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt64BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes an rgb <see cref="Color"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="Color"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteRgbColor(this Stream stream, Color value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfArgbColor];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(buffer, (value.R << 16) | (value.G << 8) | value.B);
+            }
+            else
+            {
+                BinaryPrimitives.WriteInt32BigEndian(buffer, (value.B << 16) | (value.G << 8) | value.R);
+            }
+
+            stream.Write(buffer.Slice(3));
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="float"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="float"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteSingle(this Stream stream, float value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfSingle];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteSingleLittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteSingleBigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="string"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="string"/> to be written to a stream.</param>
+        /// <param name="encoding">The encoding of the string.</param>
+        public static void WriteString(this Stream stream, string value, Encoding? encoding = null) =>
+            stream.Write((encoding ?? Encoding.Default).GetBytes(value));
+
+        /// <summary>
+        ///     Writes a <see cref="ushort"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="ushort"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteUInt16(this Stream stream, ushort value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt16];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteUInt16LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteUInt16BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="uint"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="uint"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteUInt32(this Stream stream, uint value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt32];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteUInt32LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteUInt32BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+
+        /// <summary>
+        ///     Writes a <see cref="ulong"/> to a stream.
+        /// </summary>
+        /// <param name="stream">The stream that holds the data.</param>
+        /// <param name="value">The <see cref="ulong"/> to be written to a stream.</param>
+        /// <param name="littleEndian">Whether or not the data is little endian.</param>
+        public static void WriteUInt64(this Stream stream, ulong value, bool littleEndian = true)
+        {
+            Span<byte> buffer = stackalloc byte[SizeOfUInt64];
+
+            if (littleEndian)
+            {
+                BinaryPrimitives.WriteUInt64LittleEndian(buffer, value);
+            }
+            else
+            {
+                BinaryPrimitives.WriteUInt64BigEndian(buffer, value);
+            }
+
+            stream.Write(buffer);
+        }
+    }
+}

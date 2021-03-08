3a2c35a8b65198f3cd831bb4ab2642d44e7d3e95
redageddon <therealrubiksmaster@gmail.com>
2021-03-07 19:33:48 -0800

    added next enum (yoinked and upgraded from oliverbooth)

--- X10D.Performant/src/Custom/RandomExtensions/Next.cs
+++ X10D.Performant/src/Custom/RandomExtensions/Next.cs
@@ -6,6 +6,22 @@ namespace X10D.Performant
     public static partial class RandomExtensions
     {
         /// <summary>
+        ///     Returns a random value that defined in a specified enum.
+        /// </summary>
+        /// <param name="random">The <see cref="System.Random"/> instance.</param>
+        /// <typeparam name="TEnum">An enumeration type.</typeparam>
+        /// <returns>
+        ///     Returns a random enum value from <typeparamref name="TEnum"/> specified by <paramref name="random"/>.
+        /// </returns>
+        public static TEnum NextEnum<TEnum>(this Random random)
+            where TEnum : struct, Enum
+        {
+            TEnum[] values = EnumExtensions.EnumMap<TEnum>.Map;
+
+            return values[random.Next(values.Length)];
+        }
+
+        /// <summary>
         ///     Returns either <see langword="true"/> or <see langword="false"/> based on <paramref name="random"/>'s next generation.
         /// </summary>
         /// <param name="random">The <see cref="Random"/> instance.</param>

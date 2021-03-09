af3ff8ff986e8d76fccbad58d73f36b8a46ada9b
redageddon <therealrubiksmaster@gmail.com>
2021-03-08 16:18:02 -0800

    increased the range distribution of next long and fixed possible underflow

--- X10D.Performant/src/Custom/RandomExtensions/Next.cs
+++ X10D.Performant/src/Custom/RandomExtensions/Next.cs
@@ -149,8 +149,13 @@ namespace X10D.Performant
         /// <param name="maxValue">The exclusive upper bound of the random number returned. This value must be greater than or equal to 0.</param>
         /// <returns>A random <see cref="long"/> between 0 and <paramref name="maxValue"/>.</returns>
         // TODO: remove decimal usage and ensure only 1 random.Next is called
-        public static long NextInt64(this Random random, long maxValue = long.MaxValue) =>
-            (long)(maxValue * (decimal)random.NextDouble());
+        public static long NextInt64(this Random random, long maxValue = long.MaxValue)
+        {
+            // Introduces a larger range of distribution but all values are still included so random.NextDecimal() is safe to use here.
+            decimal value = (decimal)random.Next() / long.MaxValue;
+
+            return (long)(maxValue * value);
+        }
 
         /// <summary>
         ///     Returns a random <see cref="long"/> within a specified range.
@@ -162,8 +167,13 @@ namespace X10D.Performant
         /// </param>
         /// <returns>A random <see cref="long"/> between <paramref name="minValue"/> and <paramref name="maxValue"/>.</returns>
         // TODO: remove decimal usage and ensure only 1 random.Next is called
-        public static long NextInt64(this Random random, long minValue, long maxValue) =>
-            (long)(((maxValue - minValue) * (decimal)random.NextDouble()) + minValue);
+        public static long NextInt64(this Random random, long minValue, long maxValue)
+        {
+            // Introduces a larger range of distribution but all values are still included so random.NextDecimal() is safe to use here.
+            decimal value = (decimal)random.Next() / long.MaxValue;
+
+            return (long)((maxValue * value) - (minValue * value) + minValue);
+        }
 
         /// <summary>
         ///     Returns a random <see cref="sbyte"/> within a specified range.

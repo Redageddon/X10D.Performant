aa70457b8f6813fb9f33913432ae1a169b6c5cc4
redageddon <therealrubiksmaster@gmail.com>
2021-03-09 22:16:41 -0800

    added nextRandom random extension method (random inception)

---
+++ X10D.Performant/src/Custom/RandomExtensions/Next/Random.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace X10D.Performant
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        ///     Creates a new <see cref="System.Random"/> from an already existing <see cref="System.Random"/>.
+        /// </summary>
+        /// <param name="random">The random instance getting the seed.</param>
+        /// <returns>A new <see cref="System.Random"/> instance with the seed provided by <paramref name="random"/>.</returns>
+        public static Random NextRandom(this Random random) => new(random.Next());
+    }
+}

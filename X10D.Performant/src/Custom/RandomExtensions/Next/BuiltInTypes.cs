8c666c6414d9553a421f9b120b8d5cf8c45d9598
redageddon <therealrubiksmaster@gmail.com>
2021-03-09 21:33:16 -0800

    split next into more appropriate files

--- X10D.Performant/src/Custom/RandomExtensions/Next/BuiltInTypes.cs
+++ X10D.Performant/src/Custom/RandomExtensions/Next/BuiltInTypes.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 
 namespace X10D.Performant
 {
@@ -47,64 +46,6 @@ namespace X10D.Performant
         public static char NextChar(this Random random, char minValue = 'a', char maxValue = 'z') => (char)random.Next(minValue, maxValue + 1);
 
         /// <summary>
-        ///     Creates a new random <see cref="Color"/> with the supplied hsv values.
-        /// </summary>
-        /// <param name="random">The <see cref="System.Random"/> instance that determines the randomness of the color.</param>
-        /// <param name="minAlpha">The lowest alpha value to randomize.</param>
-        /// <param name="maxAlpha">The highest alpha value to randomize.</param>
-        /// <param name="minHue">The lowest hue value to randomize.</param>
-        /// <param name="maxHue">The highest hue value to randomize.</param>
-        /// <param name="minSaturation">The lowest saturation value to randomize.</param>
-        /// <param name="maxSaturation">The highest saturation value to randomize.</param>
-        /// <param name="minBrightness">The lowest brightness value to randomize.</param>
-        /// <param name="maxBrightness">The highest brightness value to randomize.</param>
-        /// <returns>A new random color with the supplied ahsv values.</returns>
-        public static Color NextColorAhsb(this Random random,
-                                          byte minAlpha = byte.MinValue,
-                                          byte maxAlpha = byte.MaxValue,
-                                          float minHue = 0,
-                                          float maxHue = 1,
-                                          float minSaturation = 0,
-                                          float maxSaturation = 1,
-                                          float minBrightness = 0,
-                                          float maxBrightness = 1)
-        {
-            byte a = (byte)random.Next(minAlpha, maxAlpha);
-            float h = (float)random.NextDouble().Lerp(minHue, maxHue);
-            float s = (float)random.NextDouble().Lerp(minSaturation, maxSaturation);
-            float b = (float)random.NextDouble().Lerp(minBrightness, maxBrightness);
-
-            return ColorExtensions.ColorFromAhsb(a, h, s, b);
-        }
-
-        /// <summary>
-        ///     Creates a new random <see cref="Color"/> with the supplied hsv values.
-        /// </summary>
-        /// <param name="random">The <see cref="System.Random"/> instance that determines the randomness of the color.</param>
-        /// <param name="minAlpha">The lowest alpha value to randomize.</param>
-        /// <param name="maxAlpha">The highest alpha value to randomize.</param>
-        /// <param name="minRed">The lowest red value to randomize.</param>
-        /// <param name="maxRed">The highest red value to randomize.</param>
-        /// <param name="minGreen">The lowest green value to randomize.</param>
-        /// <param name="maxGreen">The highest green value to randomize.</param>
-        /// <param name="minBlue">The lowest blue value to randomize.</param>
-        /// <param name="maxBlue">The highest blue value to randomize.</param>
-        /// <returns>A new random color with the supplied argb values.</returns>
-        public static Color NextColorArgb(this Random random,
-                                          byte minAlpha = byte.MinValue,
-                                          byte maxAlpha = byte.MaxValue,
-                                          byte minRed = byte.MinValue,
-                                          byte maxRed = byte.MaxValue,
-                                          byte minGreen = byte.MinValue,
-                                          byte maxGreen = byte.MaxValue,
-                                          byte minBlue = byte.MinValue,
-                                          byte maxBlue = byte.MaxValue) =>
-            Color.FromArgb((random.Next(minAlpha, maxAlpha) << 24)
-                         | (random.Next(minRed, maxRed) << 16)
-                         | (random.Next(minGreen, maxGreen) << 8)
-                         | random.Next(minBlue, maxBlue));
-
-        /// <summary>
         ///     Returns a random <see cref="double"/> that is within a specified range.
         /// </summary>
         /// <param name="random">The <see cref="System.Random"/> instance.</param>
@@ -125,22 +66,6 @@ namespace X10D.Performant
         /// <returns>A random <see cref="double"/> between <paramref name="minValue"/> and <paramref name="maxValue"/>.</returns>
         public static double NextDouble(this Random random, double minValue, double maxValue) =>
             ((maxValue - minValue) * random.NextDouble()) + minValue;
-
-        /// <summary>
-        ///     Returns a random value that is defined in a specified enum.
-        /// </summary>
-        /// <param name="random">The <see cref="System.Random"/> instance.</param>
-        /// <typeparam name="TEnum">An enumeration type.</typeparam>
-        /// <returns>
-        ///     Returns a random enum value from <typeparamref name="TEnum"/> specified by <paramref name="random"/>.
-        /// </returns>
-        public static TEnum NextEnum<TEnum>(this Random random)
-            where TEnum : struct, Enum
-        {
-            TEnum[] values = EnumExtensions.EnumMap<TEnum>.Map;
-
-            return values[random.Next(values.Length)];
-        }
 
         /// <summary>
         ///     Returns a random <see cref="short"/> within a specified range.
---
+++ X10D.Performant/src/Custom/RandomExtensions/Next/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace X10D.Performant
+{
+    public static partial class RandomExtensions
+    {
+        /// <summary>
+        ///     Returns a random value that is defined in a specified enum.
+        /// </summary>
+        /// <param name="random">The <see cref="System.Random"/> instance.</param>
+        /// <typeparam name="TEnum">An enumeration type.</typeparam>
+        /// <returns>
+        ///     Returns a random enum value from <typeparamref name="TEnum"/> specified by <paramref name="random"/>.
+        /// </returns>
+        public static TEnum NextEnum<TEnum>(this Random random)
+            where TEnum : struct, Enum
+        {
+            TEnum[] values = EnumExtensions.EnumMap<TEnum>.Map;
+
+            return values[random.Next(values.Length)];
+        }
+    }
+}

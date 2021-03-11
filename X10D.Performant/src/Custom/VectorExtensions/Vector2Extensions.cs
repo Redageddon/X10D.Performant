5b0f83babe4e50bdd03acb6c590775f4a4ecc892
redageddon <therealrubiksmaster@gmail.com>
2021-03-11 10:37:14 -0800

    added Vector2Extensions.cs with a lerp extension

---
+++ X10D.Performant/src/Custom/VectorExtensions/Vector2Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace X10D.Performant.VectorExtensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="Vector2"/>.
+    /// </summary>
+    public static class Vector2Extensions
+    {
+        public static Vector2 Lerp(this Vector2 alpha, Vector2 firstValue, Vector2 secondValue) =>
+            new(alpha.X.Lerp(firstValue.X, secondValue.X),
+                alpha.Y.Lerp(firstValue.Y, secondValue.Y));
+    }
+}

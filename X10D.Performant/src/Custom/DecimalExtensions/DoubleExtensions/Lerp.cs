9c01e64dfcc286346181c57568840182dbb56237
redageddon <therealrubiksmaster@gmail.com>
2021-03-11 09:49:54 -0800

    added ClampedLerp

--- X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/Lerp.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/Lerp.cs
@@ -15,5 +15,23 @@
 
         /// <inheritdoc cref="Lerp"/>
         public static double LerpTo(this double firstValue, double secondValue, double alpha) => (firstValue * (1 - alpha)) + (secondValue * alpha);
+
+        /// <summary>
+        ///     Performs a clamped linear interpolation between two values.
+        /// </summary>
+        /// <param name="alpha">The percent amount to interpolate 2 values in the range of 0 to 1.</param>
+        /// <param name="firstValue">The first value to lerp to.</param>
+        /// <param name="secondValue">The second value to lerp to.</param>
+        /// <returns>
+        ///     A value that is between <paramref name="firstValue"/> and <paramref name="secondValue"/> by a percent of <paramref name="alpha"/>.
+        ///     The result cannot be less than <paramref name="firstValue"/> or greater than <paramref name="secondValue"/>.
+        /// </returns>
+        public static double ClampedLerp(this double alpha, double firstValue, double secondValue) =>
+            alpha switch
+            {
+                <= 0 => firstValue,
+                >= 1 => secondValue,
+                _    => (firstValue * (1 - alpha)) + (secondValue * alpha),
+            };
     }
 }
--- X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/Lerp.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/Lerp.cs
@@ -7,5 +7,14 @@
 
         /// <inheritdoc cref="Lerp"/>
         public static float LerpTo(this float firstValue, float secondValue, float alpha) => (firstValue * (1 - alpha)) + (secondValue * alpha);
+
+        /// <inheritdoc cref="DoubleExtensions.ClampedLerp"/>
+        public static float ClampedLerp(this float alpha, float firstValue, float secondValue) =>
+            alpha switch
+            {
+                <= 0 => firstValue,
+                >= 1 => secondValue,
+                _    => (firstValue * (1 - alpha)) + (secondValue * alpha),
+            };
     }
 }

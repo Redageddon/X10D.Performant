4bcc6ee5b8e3ea9cce50dd406b25700cf9c3d70c
redageddon <therealrubiksmaster@gmail.com>
2021-03-10 20:19:15 -0800

    added FahrenheitToCelsius extension method

---
+++ X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/UnitConversions/Temperature.cs
@@ -0,0 +1,12 @@
+﻿namespace X10D.Performant
+{
+    public static partial class DoubleExtensions
+    {
+        /// <summary>
+        ///     Converts <paramref name="fahrenheit"/> to Celsius.
+        /// </summary>
+        /// <param name="fahrenheit"></param>
+        /// <returns></returns>
+        public static double FahrenheitToCelsius(this double fahrenheit) => (fahrenheit - 32) * 5 / 9;
+    }
+}

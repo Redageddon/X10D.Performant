09b8820bde49b64c4bfbe1281e5730e9f4304b86
redageddon <therealrubiksmaster@gmail.com>
2021-03-10 20:14:00 -0800

    named angle conversion parameter names better

--- X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/UnitConversions/AngleConversions.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/UnitConversions/AngleConversions.cs
@@ -5,45 +5,45 @@ namespace X10D.Performant
     public static partial class DoubleExtensions
     {
         /// <summary>
-        ///     Converts <paramref name="angle"/> from degrees to gradians.
+        ///     Converts <paramref name="degree"/> from degrees to gradians.
         /// </summary>
-        /// <param name="angle">The angle in degrees.</param>
-        /// <returns><paramref name="angle"/> in gradians.</returns>
-        public static double DegreesToGradians(this double angle) => angle * 10D / 9D;
+        /// <param name="degree">The angle in degrees.</param>
+        /// <returns><paramref name="degree"/> in gradians.</returns>
+        public static double DegreesToGradians(this double degree) => degree * 10D / 9D;
 
         /// <summary>
-        ///     Converts <paramref name="angle"/> from degrees to radians.
+        ///     Converts <paramref name="degree"/> from degrees to radians.
         /// </summary>
-        /// <param name="angle">The angle in degrees.</param>
-        /// <returns><paramref name="angle"/> in radians.</returns>
-        public static double DegreesToRadians(this double angle) => Math.PI * angle / 180.0D;
+        /// <param name="degree">The angle in degrees.</param>
+        /// <returns><paramref name="degree"/> in radians.</returns>
+        public static double DegreesToRadians(this double degree) => Math.PI * degree / 180.0D;
 
         /// <summary>
-        ///     Converts <paramref name="angle"/> from gradians to degrees.
+        ///     Converts <paramref name="gradian"/> from gradians to degrees.
         /// </summary>
-        /// <param name="angle">The angle in gradians.</param>
-        /// <returns><paramref name="angle"/> in degrees.</returns>
-        public static double GradiansToDegrees(this double angle) => angle * 9D / 10D;
+        /// <param name="gradian">The angle in gradians.</param>
+        /// <returns><paramref name="gradian"/> in degrees.</returns>
+        public static double GradiansToDegrees(this double gradian) => gradian * 9D / 10D;
 
         /// <summary>
-        ///     Converts <paramref name="angle"/> from gradians to radians.
+        ///     Converts <paramref name="gradian"/> from gradians to radians.
         /// </summary>
-        /// <param name="angle">The angle in gradians.</param>
-        /// <returns><paramref name="angle"/> in radians.</returns>
-        public static double GradiansToRadians(this double angle) => angle * Math.PI / 200D;
+        /// <param name="gradian">The angle in gradians.</param>
+        /// <returns><paramref name="gradian"/> in radians.</returns>
+        public static double GradiansToRadians(this double gradian) => gradian * Math.PI / 200D;
 
         /// <summary>
-        ///     Converts <paramref name="angle"/> from radians to degrees.
+        ///     Converts <paramref name="radian"/> from radians to degrees.
         /// </summary>
-        /// <param name="angle">The angle in radians.</param>
-        /// <returns><paramref name="angle"/> in degrees.</returns>
-        public static double RadiansToDegrees(this double angle) => angle * 180.0D / Math.PI;
+        /// <param name="radian">The angle in radians.</param>
+        /// <returns><paramref name="radian"/> in degrees.</returns>
+        public static double RadiansToDegrees(this double radian) => radian * 180.0D / Math.PI;
 
         /// <summary>
-        ///     Converts <paramref name="angle"/> from radians to gradians.
+        ///     Converts <paramref name="radian"/> from radians to gradians.
         /// </summary>
-        /// <param name="angle">The angle in radians.</param>
-        /// <returns><paramref name="angle"/> in gradians.</returns>
-        public static double RadiansToGradians(this double angle) => angle * 200D / Math.PI;
+        /// <param name="radian">The angle in radians.</param>
+        /// <returns><paramref name="radian"/> in gradians.</returns>
+        public static double RadiansToGradians(this double radian) => radian * 200D / Math.PI;
     }
 }
--- X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/UnitConversions/AngleConversions.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/UnitConversions/AngleConversions.cs
@@ -5,21 +5,21 @@ namespace X10D.Performant
     public static partial class SingleExtensions
     {
         /// <inheritdoc cref="DoubleExtensions.DegreesToGradians"/>
-        public static float DegreesToGradians(this float angle) => angle * 10F / 9F;
+        public static float DegreesToGradians(this float degree) => degree * 10F / 9F;
 
         /// <inheritdoc cref="DoubleExtensions.DegreesToRadians"/>
-        public static float DegreesToRadians(this float angle) => MathF.PI * angle / 180.0F;
+        public static float DegreesToRadians(this float degree) => MathF.PI * degree / 180.0F;
 
         /// <inheritdoc cref="DoubleExtensions.GradiansToDegrees"/>
-        public static float GradiansToDegrees(this float angle) => angle * 9F / 10F;
+        public static float GradiansToDegrees(this float gradian) => gradian * 9F / 10F;
 
         /// <inheritdoc cref="DoubleExtensions.GradiansToRadians"/>
-        public static float GradiansToRadians(this float angle) => angle * MathF.PI / 200F;
+        public static float GradiansToRadians(this float gradian) => gradian * MathF.PI / 200F;
 
         /// <inheritdoc cref="DoubleExtensions.RadiansToDegrees"/>
-        public static float RadiansToDegrees(this float angle) => angle * 180.0F / MathF.PI;
+        public static float RadiansToDegrees(this float radian) => radian * 180.0F / MathF.PI;
 
         /// <inheritdoc cref="DoubleExtensions.RadiansToGradians"/>
-        public static float RadiansToGradians(this float angle) => angle * 200F / MathF.PI;
+        public static float RadiansToGradians(this float radian) => radian * 200F / MathF.PI;
     }
 }

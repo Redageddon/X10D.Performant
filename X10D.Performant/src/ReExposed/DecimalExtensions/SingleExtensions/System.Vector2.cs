fc1bd8bc8f8c2aab25e9cf36cb120a3b2bada0cb
redageddon <therealrubiksmaster@gmail.com>
2021-03-11 15:42:23 -0800

    Reexposed vector2 lerp

---
+++ X10D.Performant/src/ReExposed/DecimalExtensions/SingleExtensions/System.Vector2.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace X10D.Performant.ReExposed
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    public static partial class SingleExtensions
+    {
+        /// <inheritdoc cref="Vector2.Lerp"/>
+        public static Vector2 LerpTo(this float amount, Vector2 value1, Vector2 value2) => Vector2.Lerp(value1, value2, amount);
+    }
+}
---
+++ X10D.Performant/src/ReExposed/VectorExtensions/System.Vector2.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace X10D.Performant.ReExposed.VectorExtensions
+{
+    /// <summary>
+    ///     Extensions for <see cref="Vector2"/>.
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    public static class VectorExtensions
+    {
+        /// <inheritdoc cref="Vector2.Lerp"/>
+        public static Vector2 LerpTo(this Vector2 value1, Vector2 value2, float amount) => Vector2.Lerp(value1, value2, amount);
+    }
+}

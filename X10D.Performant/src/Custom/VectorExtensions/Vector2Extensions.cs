47564d98a98d939dc4d83e44352ec1063ec4e407
redageddon <therealrubiksmaster@gmail.com>
2021-03-11 10:46:07 -0800

    added Clamped__Lerp extensions

--- X10D.Performant/src/Custom/VectorExtensions/Vector2Extensions.cs
+++ X10D.Performant/src/Custom/VectorExtensions/Vector2Extensions.cs
@@ -10,5 +10,17 @@ namespace X10D.Performant.VectorExtensions
         public static Vector2 Lerp(this Vector2 alpha, Vector2 firstValue, Vector2 secondValue) =>
             new(alpha.X.Lerp(firstValue.X, secondValue.X),
                 alpha.Y.Lerp(firstValue.Y, secondValue.Y));
+
+        public static Vector2 ClampedXYLerp(this Vector2 alpha, Vector2 firstValue, Vector2 secondValue) =>
+            new(alpha.X.ClampedLerp(firstValue.X, secondValue.X),
+                alpha.Y.ClampedLerp(firstValue.Y, secondValue.Y));
+
+        public static Vector2 ClampedYLerp(this Vector2 alpha, Vector2 firstValue, Vector2 secondValue) =>
+            new(alpha.X.Lerp(firstValue.X, secondValue.X),
+                alpha.Y.ClampedLerp(firstValue.Y, secondValue.Y));
+
+        public static Vector2 ClampedXLerp(this Vector2 alpha, Vector2 firstValue, Vector2 secondValue) =>
+            new(alpha.X.ClampedLerp(firstValue.X, secondValue.X),
+                alpha.Y.Lerp(firstValue.Y, secondValue.Y));
     }
 }

0e5419c6e104d06fa4d1ec5ac2569781fb290085
redageddon <therealrubiksmaster@gmail.com>
2021-03-11 09:41:09 -0800

    added LerpTo

--- X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/Lerp.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/Lerp.cs
@@ -3,14 +3,17 @@
     public static partial class DoubleExtensions
     {
         /// <summary>
-        ///     Performs linear interpolation between two values;
+        ///     Performs linear interpolation between two values.
         /// </summary>
-        /// <param name="alpha">The percent amount to interpolate 2 values in the range of 0 to 1;</param>
+        /// <param name="alpha">The percent amount to interpolate 2 values in the range of 0 to 1.</param>
         /// <param name="firstValue">The first value to lerp to.</param>
         /// <param name="secondValue">The second value to lerp to.</param>
         /// <returns>
         ///     A value that is between <paramref name="firstValue"/> and <paramref name="secondValue"/> by a percent of <paramref name="alpha"/>.
         /// </returns>
         public static double Lerp(this double alpha, double firstValue, double secondValue) => (firstValue * (1 - alpha)) + (secondValue * alpha);
+
+        /// <inheritdoc cref="Lerp"/>
+        public static double LerpTo(this double firstValue, double secondValue, double alpha) => (firstValue * (1 - alpha)) + (secondValue * alpha);
     }
 }
--- X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/Lerp.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/Lerp.cs
@@ -4,5 +4,8 @@
     {
         /// <inheritdoc cref="DoubleExtensions.Lerp"/>
         public static float Lerp(this float alpha, float firstValue, float secondValue) => (firstValue * (1 - alpha)) + (secondValue * alpha);
+
+        /// <inheritdoc cref="Lerp"/>
+        public static float LerpTo(this float firstValue, float secondValue, float alpha) => (firstValue * (1 - alpha)) + (secondValue * alpha);
     }
 }

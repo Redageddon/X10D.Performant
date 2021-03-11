4a0428d02376cde71e548a7ec87dea7d527654af
redageddon <therealrubiksmaster@gmail.com>
2021-03-10 20:54:59 -0800

    paralleled double temperature extensions to float

---
+++ X10D.Performant/src/Custom/DecimalExtensions/SingleExtensions/UnitConversions/Temperature.cs
@@ -0,0 +1,23 @@
+﻿namespace X10D.Performant
+{
+    public static partial class SingleExtensions
+    {
+        /// <inheritdoc cref="DoubleExtensions.CelsiusToFahrenheit"/>
+        public static float CelsiusToFahrenheit(this float celsius) => (celsius * 9 / 5) + 32;
+
+        /// <inheritdoc cref="DoubleExtensions.CelsiusToKelvin"/>
+        public static float CelsiusToKelvin(this float celsius) => celsius + 273.15F;
+
+        /// <inheritdoc cref="DoubleExtensions.FahrenheitToCelsius"/>
+        public static float FahrenheitToCelsius(this float fahrenheit) => (fahrenheit - 32) * 5 / 9;
+
+        /// <inheritdoc cref="DoubleExtensions.FahrenheitToKelvin"/>
+        public static float FahrenheitToKelvin(this float fahrenheit) => ((fahrenheit - 32) * 5 / 9) + 273.15F;
+
+        /// <inheritdoc cref="DoubleExtensions.KelvinToCelsius"/>
+        public static float KelvinToCelsius(this float kelvin) => kelvin - 273.15F;
+
+        /// <inheritdoc cref="DoubleExtensions.KelvinToFahrenheit"/>
+        public static float KelvinToFahrenheit(this float kelvin) => ((kelvin - 273.15F) * 9 / 5) + 32;
+    }
+}

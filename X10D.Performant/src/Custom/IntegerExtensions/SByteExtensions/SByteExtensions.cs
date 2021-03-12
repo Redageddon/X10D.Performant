69ca9ef299a566b374787b0111b6d2cad90d782d
redageddon <therealrubiksmaster@gmail.com>
2021-03-12 14:46:07 -0800

    succumbed to sbyte prime check optimizations

--- X10D.Performant/src/Custom/IntegerExtensions/SByteExtensions/SByteExtensions.cs
+++ X10D.Performant/src/Custom/IntegerExtensions/SByteExtensions/SByteExtensions.cs
@@ -6,7 +6,7 @@ namespace X10D.Performant
     /// <summary>
     ///     Extension methods for <see cref="sbyte"/>.
     /// </summary>
-    public static class SByteExtensions
+    public static partial class SByteExtensions
     {
         /// <inheritdoc cref="Int64Extensions.FromUnixTimestamp"/>
         public static DateTime FromUnixTimestamp(this sbyte timestamp, bool isMilliseconds = false) =>
@@ -17,27 +17,6 @@ namespace X10D.Performant
 
         /// <inheritdoc cref="Int64Extensions.IsOdd"/>
         public static bool IsOdd(this sbyte value) => (value & 1) == 1;
-
-        /// <inheritdoc cref="UInt64Extensions.IsPrime"/>
-        public static bool IsPrime(this sbyte value)
-        {
-            switch (value)
-            {
-                case < 2: return false;
-                case 2:
-                case 3:
-                case 5:
-                case 7:
-                case 11: return true;
-
-                default:
-                    return Mod(value, 2) != 0
-                        && Mod(value, 3) != 0
-                        && Mod(value, 5) != 0
-                        && Mod(value, 7) != 0
-                        && Mod(value, 11) != 0;
-            }
-        }
 
         /// <inheritdoc cref="UInt64Extensions.Mod"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]

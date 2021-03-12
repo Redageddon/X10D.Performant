ee8b806df89a7a0cb05f195c0e8870e3d328a6c7
redageddon <therealrubiksmaster@gmail.com>
2021-03-12 14:07:49 -0800

    succumbed to more speed on byte prime check

--- X10D.Performant/src/Custom/IntegerExtensions/ByteExtensions/ByteExtensions.cs
+++ X10D.Performant/src/Custom/IntegerExtensions/ByteExtensions/ByteExtensions.cs
@@ -6,7 +6,7 @@ namespace X10D.Performant
     /// <summary>
     ///     Extension methods for <see cref="byte"/>.
     /// </summary>
-    public static class ByteExtensions
+    public static partial class ByteExtensions
     {
         /// <inheritdoc cref="Int64Extensions.FromUnixTimestamp"/>
         public static DateTime FromUnixTimestamp(this byte timestamp, bool isMilliseconds = false) =>
@@ -17,29 +17,6 @@ namespace X10D.Performant
 
         /// <inheritdoc cref="Int64Extensions.IsOdd"/>
         public static bool IsOdd(this byte value) => (value & 1) != 0;
-
-        /// <inheritdoc cref="Int64Extensions.IsPrime"/>
-        public static bool IsPrime(this byte value)
-        {
-            switch (value)
-            {
-                case < 2: return false;
-                case 2:
-                case 3:
-                case 5:
-                case 7:
-                case 11:
-                case 13: return true;
-
-                default:
-                    return Mod(value, 2) != 0
-                        && Mod(value, 3) != 0
-                        && Mod(value, 5) != 0
-                        && Mod(value, 7) != 0
-                        && Mod(value, 11) != 0
-                        && Mod(value, 13) != 0;
-            }
-        }
 
         /// <inheritdoc cref="UInt64Extensions.Mod"/>
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]

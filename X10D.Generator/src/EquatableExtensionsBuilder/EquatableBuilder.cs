2a082a511f389af7f0df2337737f5c702e0527fa
redageddon <therealrubiksmaster@gmail.com>
2021-03-14 10:59:06 -0700

    updated equatable extensions to build entire directory

---
+++ X10D.Generator/src/EquatableExtensionsBuilder/EquatableBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace X10D.Generator
+{
+    public partial class EquatableBuilder
+    {
+        public EquatableBuilder()
+        {
+            string andClass = GetClass("And", "&&", false, 15);
+            string nandClass = GetClass("NAnd", "&&", true, 15);
+            string orClass = GetClass("Or", "||", false, 15);
+            string norClass = GetClass("NOr", "||", true, 15);
+            string xorClass = GetClass("XOr", "^", false, 15);
+            string xnorClass = GetClass("XNOr", "^", true, 15);
+
+            Directory.CreateDirectory(Path.Combine(Program.CustomPath, "IEquatableExtensions"));
+
+            File.Copy("RawCopy/EquatableExtensions.xml",
+                      Path.Combine(Program.CustomPath, "IEquatableExtensions/EquatableExtensions.xml"), true);
+
+            File.Copy("RawCopy/AdvancedComparisonInlining.cs.copy",
+                      Path.Combine(Program.CustomPath, "IEquatableExtensions/AdvancedComparisonInlining.copy.cs"), true);
+
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/AndEquals.g.cs"), andClass);
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/NAndEquals.g.cs"), nandClass);
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/OrEquals.g.cs"), orClass);
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/NOrEquals.g.cs"), norClass);
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/XOrEquals.g.cs"), xorClass);
+            File.WriteAllText(Path.Combine(Program.CustomPath, "IEquatableExtensions/XNOrEquals.g.cs"), xnorClass);
+        }
+    }
+}
---
+++ X10D.Generator/src/EquatableExtensionsBuilder/EquatableConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace X10D.Generator
+{
+    public partial class EquatableBuilder
+    {
+        private const string StartingChunk = @"using System;
+using System.Runtime.CompilerServices;
+
+namespace X10D.Performant.IEquatableExtensions
+{
+    public static partial class EquatableExtensions
+    {
+";
+
+        private const string EndingChunk = @"    }
+}";
+    }
+}

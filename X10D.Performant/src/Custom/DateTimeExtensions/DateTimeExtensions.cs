132992069e1f19f76755fabc26210f7bc70d98de
redageddon <therealrubiksmaster@gmail.com>
2021-03-13 16:19:23 -0800

    split DateTime documentation from code

--- X10D.Performant/src/Custom/DateTimeExtensions/DateTimeExtensions.cs
+++ X10D.Performant/src/Custom/DateTimeExtensions/DateTimeExtensions.cs
@@ -7,12 +7,7 @@ namespace X10D.Performant.DateTimeExtensions
     /// </summary>
     public static class DateTimeExtensions
     {
-        /// <summary>
-        ///     Returns an <see cref="int"/> of the number of years since <see cref="DateTime"/> as of <see cref="DateTime"/>.
-        /// </summary>
-        /// <param name="date">The date from which to start.</param>
-        /// <param name="asOf">The date at which to stop counting.</param>
-        /// <returns>An <see cref="int"/> representing the number of years since <paramref name="date"/> as of <paramref name="asOf"/>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="Age"]'/>
         public static int Age(this DateTime date, DateTime? asOf = null)
         {
             const double daysInYear = 365.2425;
@@ -21,12 +16,7 @@ namespace X10D.Performant.DateTimeExtensions
             return (int)((offset.TotalDays + 1) / daysInYear);
         }
 
-        /// <summary>
-        ///     Gets a <see cref="DateTime"/> representing the first occurence of a specified day in the current month.
-        /// </summary>
-        /// <param name="current">The current day.</param>
-        /// <param name="dayOfWeek">The current day of week.</param>
-        /// <returns>A <see cref="DateTime"/> representing the first occurence of <paramref name="dayOfWeek"/>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="First"]'/>
         public static DateTime First(this DateTime current, DayOfWeek dayOfWeek)
         {
             DateTime first = current.FirstDayOfMonth();
@@ -39,19 +29,10 @@ namespace X10D.Performant.DateTimeExtensions
             return first;
         }
 
-        /// <summary>
-        ///     Gets a <see cref="DateTime"/> representing the first day in the current month.
-        /// </summary>
-        /// <param name="current">The current date.</param>
-        /// <returns>A <see cref="DateTime"/> representing the first day of the month>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="FirstDayOfMonth"]'/>
         public static DateTime FirstDayOfMonth(this DateTime current) => current.AddDays(1 - current.Day);
 
-        /// <summary>
-        ///     Gets a <see cref="DateTime"/> representing the last specified day in the current month.
-        /// </summary>
-        /// <param name="current">The current date.</param>
-        /// <param name="dayOfWeek">The current day of week.</param>
-        /// <returns>A <see cref="DateTime"/> representing the final occurence of <paramref name="dayOfWeek"/>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="Last"]'/>
         public static DateTime Last(this DateTime current, DayOfWeek dayOfWeek)
         {
             DateTime last = current.LastDayOfMonth();
@@ -63,11 +44,7 @@ namespace X10D.Performant.DateTimeExtensions
             return last.AddDays(offset);
         }
 
-        /// <summary>
-        ///     Gets a <see cref="DateTime"/> representing the last day in the current month.
-        /// </summary>
-        /// <param name="current">The current date.</param>
-        /// <returns>A <see cref="DateTime"/> representing the last day of the month>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="LastDayOfMonth"]'/>
         public static DateTime LastDayOfMonth(this DateTime current)
         {
             int daysInMonth = DateTime.DaysInMonth(current.Year, current.Month);
@@ -75,12 +52,7 @@ namespace X10D.Performant.DateTimeExtensions
             return new DateTime(current.Year, current.Month, daysInMonth);
         }
 
-        /// <summary>
-        ///     Gets a <see cref="DateTime"/> representing the first date following the current date which falls on the given day of the week.
-        /// </summary>
-        /// <param name="current">The current date.</param>
-        /// <param name="dayOfWeek">The day of week for the next date to get.</param>
-        /// <returns>A <see cref="DateTime"/> representing the next occurence of <paramref name="dayOfWeek"/>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="Next"]'/>
         public static DateTime Next(this DateTime current, DayOfWeek dayOfWeek)
         {
             int offsetDays = dayOfWeek - current.DayOfWeek;
@@ -93,14 +65,7 @@ namespace X10D.Performant.DateTimeExtensions
             return current.AddDays(offsetDays);
         }
 
-        /// <summary>
-        ///     Converts the <see cref="DateTime"/> to a Unix timestamp.
-        /// </summary>
-        /// <param name="time">The <see cref="DateTime"/> instance.</param>
-        /// <param name="useMillis">
-        ///     Whether or not the return value should be represented as milliseconds. Defaults to <see langword="false"/>.
-        /// </param>
-        /// <returns>A <see cref="long"/> in terms of a Unix timestamp representing the provided <see cref="DateTime"/>.</returns>
+        /// <include file='DateTimeExtensions.xml' path='members/member[@name="ToUnixTimeStamp"]'/>
         public static long ToUnixTimeStamp(this DateTime time, bool useMillis = false)
         {
             DateTimeOffset offset = time;

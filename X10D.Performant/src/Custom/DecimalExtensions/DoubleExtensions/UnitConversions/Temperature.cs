8e8084da1c10eb34991cf972829d95d463da1d6a
redageddon <therealrubiksmaster@gmail.com>
2021-03-14 11:47:13 -0700

    added explicit floating point type

--- X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/UnitConversions/Temperature.cs
+++ X10D.Performant/src/Custom/DecimalExtensions/DoubleExtensions/UnitConversions/Temperature.cs
@@ -6,18 +6,18 @@
         public static double CelsiusToFahrenheit(this double celsius) => (celsius * 9 / 5) + 32;
 
         /// <include file='../DoubleExtensions.xml' path='members/member[@name="CelsiusToKelvin"]'/>
-        public static double CelsiusToKelvin(this double celsius) => celsius + 273.15;
+        public static double CelsiusToKelvin(this double celsius) => celsius + 273.15D;
 
         /// <include file='../DoubleExtensions.xml' path='members/member[@name="FahrenheitToCelsius"]'/>
         public static double FahrenheitToCelsius(this double fahrenheit) => (fahrenheit - 32) * 5 / 9;
 
         /// <include file='../DoubleExtensions.xml' path='members/member[@name="FahrenheitToKelvin"]'/>
-        public static double FahrenheitToKelvin(this double fahrenheit) => ((fahrenheit - 32) * 5 / 9) + 273.15;
+        public static double FahrenheitToKelvin(this double fahrenheit) => ((fahrenheit - 32) * 5 / 9) + 273.15D;
 
         /// <include file='../DoubleExtensions.xml' path='members/member[@name="KelvinToCelsius"]'/>
-        public static double KelvinToCelsius(this double kelvin) => kelvin - 273.15;
+        public static double KelvinToCelsius(this double kelvin) => kelvin - 273.15D;
 
         /// <include file='../DoubleExtensions.xml' path='members/member[@name="KelvinToFahrenheit"]'/>
-        public static double KelvinToFahrenheit(this double kelvin) => ((kelvin - 273.15) * 9 / 5) + 32;
+        public static double KelvinToFahrenheit(this double kelvin) => ((kelvin - 273.15D) * 9 / 5) + 32;
     }
 }

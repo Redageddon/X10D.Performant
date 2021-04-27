e0403198cd52298dde6110b368595f5dc5fbe991
redageddon <therealrubiksmaster@gmail.com>
2021-04-27 16:14:51 -0700

    added SelectWhere

--- X10D.Performant/src/Custom/IEnumerableExtensions/EnumerableExtensions.cs
+++ X10D.Performant/src/Custom/IEnumerableExtensions/EnumerableExtensions.cs
@@ -50,5 +50,19 @@ namespace X10D.Performant.IEnumerableExtensions
                 yield return array;
             }
         }
+
+        /// <include file='EnumerableExtensions.xml' path='members/member[@name="SelectWhere"]'/>
+        public static IEnumerable<TSource> SelectWhere<TSource>(this IEnumerable<TSource> values,
+                                                                Func<TSource, TSource> selector,
+                                                                Predicate<TSource> predicate)
+        {
+            foreach (TSource source in values)
+            {
+                if (predicate(source))
+                {
+                    yield return selector(source);
+                }
+            }
+        }
     }
 }

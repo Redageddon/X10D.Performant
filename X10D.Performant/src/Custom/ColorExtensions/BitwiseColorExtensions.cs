331f91f6f9313a9fbcacc65f39d796ff38df7309
redageddon <therealrubiksmaster@gmail.com>
2021-04-28 17:19:22 -0700

    added partial to color

---
+++ X10D.Performant/src/Custom/ColorExtensions/BitwiseColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace X10D.Performant.ColorExtensions
+{
+    public static partial class ColorExtensions
+    {
+        /// <include file='ColorExtensions.xml' path='members/member[@name="And"]'/>
+        public static Color And(this Color color, Color other) => Color.FromArgb(color.ToArgb() & other.ToArgb());
+
+        /// <include file='ColorExtensions.xml' path='members/member[@name="Or"]'/>
+        public static Color Or(this Color color, Color other) => Color.FromArgb(color.ToArgb() | other.ToArgb());
+
+        /// <include file='ColorExtensions.xml' path='members/member[@name="XOr"]'/>
+        public static Color XOr(this Color color, Color other) => Color.FromArgb(color.ToArgb() ^ other.ToArgb());
+    }
+}

cce7d05425ecf05ef9da660afcd7d61057b971d6
redageddon <therealrubiksmaster@gmail.com>
2021-04-29 13:58:08 -0700

    added consume count override

--- X10D.Performant/src/Custom/IEnumerableExtensions/EnumerableExtensions.cs
+++ X10D.Performant/src/Custom/IEnumerableExtensions/EnumerableExtensions.cs
@@ -9,11 +9,11 @@ namespace X10D.Performant.IEnumerableExtensions
     public static class EnumerableExtensions
     {
         /// <include file='EnumerableExtensions.xml' path='members/member[@name="Consume"]'/>
-        public static void Consume<TSource>(this IEnumerable<TSource> values)
+        public static void Consume<TSource>(this IEnumerable<TSource> values, int consumeCount = int.MaxValue)
         {
             using IEnumerator<TSource> enumerator = values.GetEnumerator();
 
-            while (enumerator.MoveNext()) {}
+            while (consumeCount-- > 0 && enumerator.MoveNext()) {}
         }
 
         /// <include file='EnumerableExtensions.xml' path='members/member[@name="DistinctBy"]'/>

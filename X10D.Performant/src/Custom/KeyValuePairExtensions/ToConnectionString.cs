8dcee011b57b2c3734d71c679f88bfd96b78ab22
redageddon <therealrubiksmaster@gmail.com>
2021-05-16 15:37:04 -0700

    updated KeyValuePairExtensions

---
+++ X10D.Performant/src/Custom/KeyValuePairExtensions/ToConnectionString.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace X10D.Performant.KeyValuePairExtensions
+{
+    public static partial class KeyValuePairExtensions
+    {
+        /// <include file='KeyValuePairExtensions.xml' path='members/member[@name="ToConnectionString"]'/>
+        [SuppressMessage("ReSharper", "ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator")]
+        public static string ToConnectionString<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs)
+        {
+            static string? SanitizeValue(TValue value)
+            {
+                if (value is null)
+                {
+                    return string.Empty;
+                }
+
+                if (value is string str)
+                {
+                    foreach (char t in str)
+                    {
+                        if (char.IsWhiteSpace(t))
+                        {
+                            return $"\"{str}\"";
+                        }
+                    }
+                }
+
+                return value.ToString();
+            }
+
+            IEnumerable<string> InternalIterator()
+            {
+                foreach ((TKey key, TValue value) in keyValuePairs)
+                {
+                    yield return $"{key}={SanitizeValue(value)}";
+                }
+            }
+
+            return string.Join(";", InternalIterator());
+        }
+    }
+}

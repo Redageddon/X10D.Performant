ad80332850a9e3501bbbfd679cd6b1e1407d6dc2
redageddon <therealrubiksmaster@gmail.com>
2021-05-16 22:19:55 -0700

    updated shuffled to reduce code repetition

--- X10D.Performant/src/Custom/SpanExtensions/Shuffled.cs
+++ X10D.Performant/src/Custom/SpanExtensions/Shuffled.cs
@@ -5,17 +5,14 @@ namespace X10D.Performant.SpanExtensions
     public static partial class SpanExtensions
     {
         /// <include file='SpanExtensions.xml' path='members/member[@name="Shuffled"]'/>
-        public static ReadOnlySpan<T> Shuffled<T>(this ReadOnlySpan<T> values, Random? random = null)
-        {
-            Span<T> writeableSpan = new T[values.Length];
-            values.CopyTo(writeableSpan);
-            writeableSpan.Shuffle(random);
-
-            return writeableSpan;
-        }
+        public static ReadOnlySpan<T> Shuffled<T>(this ReadOnlySpan<T> values, Random? random = null) =>
+            ShuffledInternal(values, random);
 
         /// <include file='SpanExtensions.xml' path='members/member[@name="Shuffled"]'/>
-        public static Span<T> Shuffled<T>(this Span<T> values, Random? random = null)
+        public static Span<T> Shuffled<T>(this Span<T> values, Random? random = null) =>
+            ShuffledInternal((ReadOnlySpan<T>)values, random);
+
+        private static Span<T> ShuffledInternal<T>(ReadOnlySpan<T> values, Random? random)
         {
             Span<T> writeableSpan = new T[values.Length];
             values.CopyTo(writeableSpan);

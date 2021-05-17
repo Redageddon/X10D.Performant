934b4a30a08ab723641de88688678fb2456a3e21
redageddon <therealrubiksmaster@gmail.com>
2021-05-16 22:13:56 -0700

    Added span.where extensions and tests

--- X10D.Performant.Tests/src/Core/SpanTests.cs
+++ X10D.Performant.Tests/src/Core/SpanTests.cs
@@ -62,5 +62,81 @@ namespace X10D.Performant.Tests.Core
 
             CollectionAssert.AreNotEqual(span.ToArray(), buffer.ToArray());
         }
+
+        /// <summary>
+        ///     Tests for <see cref="SpanExtensions.Where{T}(ReadOnlySpan{T},Predicate{T},int)"/>
+        /// </summary>
+        [Test]
+        public void Where()
+        {
+            ReadOnlySpan<int> span = stackalloc int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8 }, span.Where(e => e % 2 == 0).ToArray());
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, span.Where(e => e % 2 == 0, 3).ToArray());
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="SpanExtensions.Where{T}(Span{T},Predicate{T},int)"/>
+        /// </summary>
+        [Test]
+        public void Where2()
+        {
+            Span<int> span = stackalloc int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8 }, span.Where(e => e % 2 == 0).ToArray());
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, span.Where(e => e % 2 == 0, 3).ToArray());
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="SpanExtensions.Where{T}(System.ReadOnlySpan{T},System.Predicate{T},ref System.Span{T},int)"/>
+        /// </summary>
+        [Test]
+        public void Where3()
+        {
+            ReadOnlySpan<int> span = stackalloc int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Span<int> buffer = stackalloc int[5];
+            span.Where(e => e % 2 == 0, ref buffer);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8 }, buffer.ToArray());
+
+            buffer = stackalloc int[5];
+            span.Where(e => e % 2 == 0, ref buffer, 3);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, buffer.ToArray());
+
+            buffer = stackalloc int[3];
+            span.Where(e => e % 2 == 0, ref buffer);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, buffer.ToArray());
+
+            buffer = stackalloc int[3];
+            span.Where(e => e % 2 == 0, ref buffer, 2);
+            CollectionAssert.AreEqual(new[] { 0, 2 }, buffer.ToArray());
+
+            buffer = stackalloc int[3];
+            span.Where(e => e % 2 == 0, ref buffer, 4);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, buffer.ToArray());
+        }
+
+        /// <summary>
+        ///     Tests for <see cref="SpanExtensions.Where{T}(Span{T},Predicate{T},ref Span{T},int)"/>
+        /// </summary>
+        [Test]
+        public void Where4()
+        {
+            Span<int> span = stackalloc int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Span<int> buffer = stackalloc int[5];
+            span.Where(e => e % 2 == 0, ref buffer);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4, 6, 8 }, buffer.ToArray());
+
+            span.Where(e => e % 2 == 0, ref buffer, 3);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, buffer.ToArray());
+
+            buffer = stackalloc int[3];
+            span.Where(e => e % 2 == 0, ref buffer);
+            CollectionAssert.AreEqual(new[] { 0, 2, 4 }, buffer.ToArray());
+
+            span.Where(e => e % 2 == 0, ref buffer, 2);
+            CollectionAssert.AreEqual(new[] { 0, 2 }, buffer.ToArray());
+        }
     }
 }
---
+++ X10D.Performant/src/Custom/SpanExtensions/Where.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    //TODO: DOCUMENT
+    public static partial class SpanExtensions
+    {
+        private const int NoValuePassed = -1;
+
+        public static ReadOnlySpan<T> Where<T>(this ReadOnlySpan<T> values, Predicate<T> predicate, int cutOffLength = NoValuePassed) =>
+            InternalWhere(values, predicate, cutOffLength);
+
+        public static Span<T> Where<T>(this Span<T> values, Predicate<T> predicate, int cutOffLength = NoValuePassed) =>
+            InternalWhere(values, predicate, cutOffLength);
+
+        public static void Where<T>(this ReadOnlySpan<T> values, Predicate<T> predicate, ref Span<T> buffer, int cutOffLength = NoValuePassed)
+        {
+            int bufferIndex = 0;
+            int valueIndex = 0;
+
+            while (valueIndex < values.Length
+                && bufferIndex < buffer.Length
+                && bufferIndex != cutOffLength)
+            {
+                T current = values[valueIndex];
+
+                if (predicate(current))
+                {
+                    buffer[bufferIndex++] = current;
+                }
+
+                valueIndex++;
+            }
+
+            buffer = buffer[..bufferIndex];
+        }
+
+        public static void Where<T>(this Span<T> values, Predicate<T> predicate, ref Span<T> buffer, int cutOffLength = NoValuePassed) =>
+            Where((ReadOnlySpan<T>)values, predicate, ref buffer, cutOffLength);
+
+        private static Span<T> InternalWhere<T>(ReadOnlySpan<T> values, Predicate<T> predicate, int cutOffLength)
+        {
+            if (cutOffLength == NoValuePassed)
+            {
+                cutOffLength = values.Length;
+            }
+
+            Span<T> result = new T[cutOffLength];
+            Where(values, predicate, ref result, cutOffLength);
+
+            return result;
+        }
+    }
+}

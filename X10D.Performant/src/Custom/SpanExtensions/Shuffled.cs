aceea5863ce6c67e8f7c7dc8e7ba9d79eedae348
redageddon <therealrubiksmaster@gmail.com>
2021-05-19 15:28:36 -0700

    added buffer overload to shuffled

--- X10D.Performant/src/Custom/SpanExtensions/Shuffled.cs
+++ X10D.Performant/src/Custom/SpanExtensions/Shuffled.cs
@@ -2,6 +2,7 @@
 
 namespace X10D.Performant.SpanExtensions
 {
+    //TODO: DOCUMENT
     public static partial class SpanExtensions
     {
         /// <include file='SpanExtensions.xml' path='members/member[@name="Shuffled"]'/>
@@ -10,11 +11,19 @@ namespace X10D.Performant.SpanExtensions
         /// <include file='SpanExtensions.xml' path='members/member[@name="Shuffled"]'/>
         public static Span<T> Shuffled<T>(this Span<T> values, Random? random = null) => ShuffledInternal((ReadOnlySpan<T>)values, random);
 
+        public static void Shuffled<T>(this ReadOnlySpan<T> values, ref Span<T> buffer, Random? random = null)
+        {
+            values.CopyTo(buffer);
+            buffer.Shuffle(random);
+        }
+
+        public static void Shuffled<T>(this Span<T> values, ref Span<T> buffer, Random? random = null) =>
+            Shuffled((ReadOnlySpan<T>)values, ref buffer, random);
+
         private static Span<T> ShuffledInternal<T>(ReadOnlySpan<T> values, Random? random)
         {
             Span<T> writeableSpan = new T[values.Length];
-            values.CopyTo(writeableSpan);
-            writeableSpan.Shuffle(random);
+            Shuffled(values, ref writeableSpan, random);
 
             return writeableSpan;
         }

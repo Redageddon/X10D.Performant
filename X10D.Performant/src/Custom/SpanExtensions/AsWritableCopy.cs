f494682c46a6095fa2cf12f9a004245affd194e0
redageddon <therealrubiksmaster@gmail.com>
2021-05-19 21:12:28 -0700

    added AsWritableCopy extension method

---
+++ X10D.Performant/src/Custom/SpanExtensions/AsWritableCopy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    //TODO: DOCUMENT
+    //TODO: TEST
+    public static partial class SpanExtensions
+    {
+        public static Span<T> AsWritableCopy<T>(this ReadOnlySpan<T> values)
+        {
+            Span<T> copy = new T[values.Length];
+            values.CopyTo(copy);
+
+            return copy;
+        }
+    }
+}

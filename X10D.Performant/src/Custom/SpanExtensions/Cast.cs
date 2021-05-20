4055face463275acf859e4502c49ad0ce24355d8
redageddon <therealrubiksmaster@gmail.com>
2021-05-19 21:48:07 -0700

    added cast span extension method

---
+++ X10D.Performant/src/Custom/SpanExtensions/Cast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    //TODO: DOCUMENT
+    //TODO: TEST
+    public static partial class SpanExtensions
+    {
+        public static ReadOnlySpan<TResult?> Cast<T, TResult>(this ReadOnlySpan<T?> values) => CastInternal<T?, TResult?>(values);
+
+        public static Span<TResult?> Cast<T, TResult>(this Span<T?> values) => CastInternal<T?, TResult?>(values);
+
+        public static void Cast<T, TResult>(this ReadOnlySpan<T?> values, ref Span<TResult?> buffer)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                buffer[i] = (TResult?)(object?)values[i];
+            }
+        }
+
+        public static void Cast<T, TResult>(this Span<T?> values, ref Span<TResult?> buffer) => Cast(values.AsReadOnly(), ref buffer);
+
+        private static Span<TResult?> CastInternal<T, TResult>(this ReadOnlySpan<T?> values)
+        {
+            Span<TResult?> result = new TResult[values.Length];
+            Cast<T?, TResult?>(values, ref result);
+
+            return result;
+        }
+    }
+}

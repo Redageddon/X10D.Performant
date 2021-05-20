76cfe8f2c8ac71be7650d5ccef046d96116e7a9f
redageddon <therealrubiksmaster@gmail.com>
2021-05-19 20:27:40 -0700

    added append span methods

---
+++ X10D.Performant/src/Custom/SpanExtensions/Append.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    //TODO: DOCUMENT
+    public static partial class SpanExtensions
+    {
+        public static ReadOnlySpan<T> Append<T>(this ReadOnlySpan<T> values, T value) => AppendInternal(values, value);
+
+        public static Span<T> Append<T>(this Span<T> values, T value) => AppendInternal(values, value);
+
+        public static void Append<T>(this ReadOnlySpan<T> values, T value, ref Span<T> buffer)
+        {
+            values.CopyTo(buffer);
+            buffer[values.Length + 1] = value;
+        }
+
+        public static void Append<T>(this Span<T> values, T value, ref Span<T> buffer) =>
+            Append((ReadOnlySpan<T>)values, value, ref buffer);
+
+        private static Span<T> AppendInternal<T>(this ReadOnlySpan<T> values, T value)
+        {
+            Span<T> output = new T[values.Length + 1];
+            Append(values, value, ref output);
+
+            return output;
+        }
+    }
+}

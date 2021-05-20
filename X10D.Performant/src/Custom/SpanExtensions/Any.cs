f458b3edec694180818c4ad16f13804d742b906c
redageddon <therealrubiksmaster@gmail.com>
2021-05-19 20:21:37 -0700

    added any span extension

---
+++ X10D.Performant/src/Custom/SpanExtensions/Any.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace X10D.Performant.SpanExtensions
+{
+    //TODO: DOCUMENT
+    public static partial class SpanExtensions
+    {
+        public static bool Any<T>(ReadOnlySpan<T> values, Predicate<T> predicate)
+        {
+            foreach (T value in values)
+            {
+                if (predicate(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool Any<T>(Span<T> values, Predicate<T> predicate) => All((ReadOnlySpan<T>)values, predicate);
+    }
+}

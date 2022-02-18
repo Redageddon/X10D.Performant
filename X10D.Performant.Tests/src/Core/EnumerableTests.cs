49ede18c87fecff7c81a4cda7f41831867e9cb29
redageddon <therealrubiksmaster@gmail.com>
2022-02-18 13:37:41 -0800

    removed tests for lazy chunk

--- X10D.Performant.Tests/src/Core/EnumerableTests.cs
+++ X10D.Performant.Tests/src/Core/EnumerableTests.cs
@@ -103,29 +103,6 @@ public class EnumerableTests
     }
 
     /// <summary>
-    ///     Tests for <see cref="IEnumerableExtensions.EnumerableExtensions.LazyChunk{T}"/>.
-    /// </summary>
-    [Test]
-    public void LazyChunk()
-    {
-        byte[] foo = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
-        IEnumerable<IEnumerable<byte>> chunks = foo.LazyChunk(2).ToArray();
-
-        Assert.AreEqual(4, chunks.Count());
-        CollectionAssert.AreEqual(new byte[] { 0x01, 0x02 }, chunks.ElementAt(0).ToList());
-        CollectionAssert.AreEqual(new byte[] { 0x03, 0x04 }, chunks.ElementAt(1).ToList());
-        CollectionAssert.AreEqual(new byte[] { 0x05, 0x06 }, chunks.ElementAt(2).ToList());
-        CollectionAssert.AreEqual(new byte[] { 0x07, 0x08 }, chunks.ElementAt(3).ToList());
-
-        IEnumerable<IEnumerable<byte>> chunks2 = foo.LazyChunk(3).ToArray();
-
-        Assert.AreEqual(3, chunks2.Count());
-        CollectionAssert.AreEqual(new byte[] { 0x01, 0x02, 0x03 }, chunks2.ElementAt(0).ToList());
-        CollectionAssert.AreEqual(new byte[] { 0x04, 0x05, 0x06 }, chunks2.ElementAt(1).ToList());
-        CollectionAssert.AreEqual(new byte[] { 0x07, 0x08, 0x00 }, chunks2.ElementAt(2).ToList());
-    }
-
-    /// <summary>
     ///     Tests for <see cref="IEnumerableExtensions.EnumerableExtensions.SelectWhere{T}"/>.
     /// </summary>
     [Test]
